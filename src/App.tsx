2507239affa2e0b84a0aee2d23559556fd3bc12b
wiru123321 <wiru123321@gmail.com>
2022-07-02 12:27:52 +0200

    Move fetch to offersCards file

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import VacationOffers from './pages/vacationOffers';
 
 function App() {
-	const [offers, setOffers] = useState([])
-
-	const fetchOffers = async () => {
-		try {
-			const res = await fetch('/api/offers', {
-				method: 'GET',
-			})
-			const json = await res.json()
-			setOffers(json.offers)
-			console.log(json.offers)
-		} catch (err) {
-			console.log(err)
-		}
-	}
-
-	useEffect(() => {
-		fetchOffers()
-	}, [])
-
 	return (
 		<div className="App">
 			<VacationOffers />
--- src/components/offersCards/index.tsx
+++ src/components/offersCards/index.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import OfferCard from './offerCard';
 
+type OffersTypes = {
+	id: number
+	subHeader: string
+	header: string
+	stars: number
+	actualPrice: string
+	lastPrice: string
+	image: string
+}
+
 const OffersCards = () => {
+	const [offers, setOffers] = useState<Array<OffersTypes>>([])
+
+	const fetchOffers = async () => {
+		try {
+			const res = await fetch('/api/offers', {
+				method: 'GET',
+			})
+			const json = await res.json()
+			setOffers(json.offers)
+			console.log(json.offers)
+		} catch (err) {
+			console.log(err)
+		}
+	}
+
+	useEffect(() => {
+		fetchOffers()
+	}, [])
 	return (
 		<div><OfferCard /></div>
 	)
